0d3cc854ac450104b93cb9f0fac9c4bcaa3cad79
aref <devfera14@gmail.com>
2025-08-29 13:36:26 -0300

    Initial Commit

---
+++ CurseList/CurseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurseListApp.swift
+//  CurseList
+//
+//  Created by arefdeveloper on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CurseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
